32ef2d835dd503ff528981fecc046b1fcf03d108
Kai Huang <kaihuang9397@gmail.com>
2018-10-18 14:27:56 -0700

    finished side nav bar

--- js/sideNav.js
+++ js/sideNav.js
@@ -1,9 +1,13 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
 	document.getElementById("mySidenav").style.width = "250px";
+	document.getElementById("main").style.transition = "0.5s";
+	document.getElementById("main").style.opacity = "0.5";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
 	document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("main").style.transition = "0.5s";
+	document.getElementById("main").style.opacity = "1.0";
 }
